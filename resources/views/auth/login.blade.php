bb9d453138f26102ec29bf1b02c70cc670c29b79
Never Lozano <nlozanob@ufpso.edu.co>
2024-03-22 22:23:06 -0500

    solucion a register

---
+++ resources/views/auth/login.blade.php
@@ -0,0 +1,59 @@
+@extends('layouts.applogin')
+@section('title', 'Login')
+@section('content')
+
+<div class="login-box">
+  <!-- /.login-logo -->
+  <div class="card card-outline card-primary">
+    <div class="card-header text-center">
+      <a href="{{asset('backend/index2.html')}}" class="h1"><b>PARQUEADERO</b></a>
+    </div>
+    <div class="card-body">
+      <p class="login-box-msg">Iniciar Sesión</p>
+
+      <form method="POST" action="{{ route('login') }}">
+            @csrf
+        <div class="input-group mb-3">
+        <input id="email" type="email" class="form-control @error('email') is-invalid @enderror" name="email" value="{{ old('email') }}" required autocomplete="email" autofocus placeholder= "correo@example.com">
+          <div class="input-group-append">
+            <div class="input-group-text">
+              <span class="fas fa-envelope"></span>
+            </div>
+          </div>
+        </div>
+        <div class="input-group mb-3">
+        <input id="password" type="password" class="form-control @error('password') is-invalid @enderror" name="password" required autocomplete="current-password">
+          <div class="input-group-append">
+            <div class="input-group-text">
+              <span class="fas fa-lock"></span>
+            </div>
+          </div>
+        </div>
+        <div class="row">
+          <!-- /.col -->
+          <div class="col-4">
+            <button type="submit" class="btn btn-primary btn-block">Ingresar</button>
+          </div>
+          <div class="col-4">
+            <a href="{{route('register')}}" class="btn btn-primary btn-block">Registrar</a>
+          </div>
+          <!-- /.col -->
+        </div>
+      </form>
+      <br>
+      <div class= "row">
+        <div class= "col-12">
+          <p>
+            @if(Route::has('password.request'))
+            <a href="{{route('password.request')}}">Olvide mi contraseña</a>
+            @endif
+          </p>
+        </div>
+      </div>
+    </div>
+    <!-- /.card-body -->
+  </div>
+  <!-- /.card -->
+</div>
+<!-- /.login-box -->
+@endsection
---
+++ resources/views/auth/register.blade.php
@@ -0,0 +1,67 @@
+@extends('layouts.applogin')
+
+@section('content')
+
+<div class="register-box bg-dark.bg-gradient text-center" >
+  <div class="card card-outline">
+    <div class="card-header text-center" style="background-color: #FBF9F1;">
+      <a href="#">
+      <img src="{{asset('backend/dist/img/prueba2.png')}}" style="width: 60%; height: auto;">
+      </a>
+    </div>
+    <div class="card-body" style="background-color: #FBF9F1;">
+      <p class="login-box-msg" style="font-weight: bold;">Register a new membership</p>
+
+      <form action="{{ route('register') }}" method="post">
+        @csrf
+        <div class="input-group mb-3 ">
+          <input id="name" type="text" class="form-control @error('name') is-invalid @enderror" name="name" value="{{ old('name') }}" required autocomplete="name" autofocus placeholder="Full Name">
+          <div class="input-group-append">
+            <div class="input-group-text">
+              <span class="fas fa-user"></span>
+            </div>
+          </div>
+        </div>
+        <div class="input-group mb-3">
+          <input id="email" type="email" class="form-control @error('email') is-invalid @enderror" name="email" value="{{ old('email') }}" required autocomplete="email" placeholder="Email">
+          <div class="input-group-append">
+            <div class="input-group-text">
+              <span class="fas fa-envelope"></span>
+            </div>
+          </div>
+        </div>
+        <div class="input-group mb-3">
+          <input id="password" type="password" class="form-control @error('password') is-invalid @enderror" name="password" required autocomplete="new-password" placeholder="Password">
+          <div class="input-group-append">
+            <div class="input-group-text">
+              <span class="fas fa-lock"></span>
+            </div>
+          </div>
+        </div>
+        <div class="input-group mb-3">
+          <input id="password-confirm" type="password" class="form-control" name="password_confirmation" required autocomplete="new-password" placeholder="Confirm Password">
+          <div class="input-group-append">
+            <div class="input-group-text">
+              <span class="fas fa-lock"></span>
+            </div>
+          </div>
+        </div>
+        <div class="row text-center">
+          <!-- /.col -->
+          <div class="col-4 text-center">
+            <button type="submit" class="btn btn-light btn-block" style="background-color: #92C7CF; color:white">Register</button>
+          </div>
+          <!-- /.col -->
+        </div>
+      </form>
+
+      <div class="social-auth-links text-center">
+
+      </div>
+
+      <a href="login" class="text-center" style="color: black;">I already have an account</a>
+    </div>
+    <!-- /.form-box -->
+  </div><!-- /.card -->
+</div>
+@endsection
