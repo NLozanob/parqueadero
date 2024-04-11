847850a47c41e5eb5e1b23b988d01023397fc103
Never Lozano <nlozanob@ufpso.edu.co>
2024-04-11 09:15:58 -0500

    Trabajo con el archivo web.php y la creciacion de los controladores

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,10 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\HomeController;
+use App\Http\Controllers\ProductController;
+use App\Http\Controllers\CustomerController;
+use App\Http\Controllers\OrderController;
 
 /*
 |--------------------------------------------------------------------------
@@ -18,6 +22,42 @@ Route::get('/', function () {
 });
 
 Auth::routes();
+
+Route::group(['middleware'=> ['auth']], function(){
+    //Panel de control
+    Route::get('/home', [App\Hppt\Controllers\HomeController::class, 'index'])->name('home');
+    //Products product
+    Route::resource('products', ProductController::class);
+    //Customers customer
+    Route::resource('customers', ProductController::class);
+    //Orders order
+    Route::resource('orders', ProductController::class);
+});
+
+Route::get('/about', function () { 
+    return 'Acerca de nosotros'; 
+});
+
+Route::get('/user/{id}', function ($id) { 
+    return 'ID de usuario: ' . $id; 
+});
+
+Route::get('/contacto', function () { 
+    return 'Página de contacto'; 
+})->name('contacto');
+
+Route::get('/usuario/{id}', function ($id) {
+    return 'ID de usuario: ' . $id;
+})->where('id', '[0-9]{3}');
+
+Route::prefix('admin')->group(function () { 
+    Route::get('/', function () { 
+        return 'Panel de administración'; 
+    }); 
+    Route::get('/users', function () { 
+        return 'Lista de usuarios'; 
+    }); 
+});
 
 Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
 
